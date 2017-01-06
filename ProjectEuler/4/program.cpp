4e538f8e350ee7c3fd3dcd9980efbfc21a18ed23
Julio C Andrade-Merrow <jcamvz@gmail.com>
2017-01-06 13:34:19 -0600

    Multiplies 100 * 100 increasing to 999 and finds all palindromes

--- ProjectEuler/4/program.cpp
+++ ProjectEuler/4/program.cpp
@@ -9,22 +9,24 @@ int main () {
         num,
         digit,
         rev = 0;
+        
+    for (int i = 100; i < 1000; i++)
+    {
+        for (int j=100; j < 1000; j++)
+        {
+            n = i*j;
+            num = n;
+            do {
+                digit = num % 10; // set digit to LAST NUMBER OF NUM
+                rev = (rev * 10) + digit; // multiplies by 10 and adds the next digit to start reversing it 
+                num = num / 10; // Take away the last digit
+            } while (num !=0);
+            if (n == rev)
+                cout << n << endl;
 
-    cout << "Enter a pos number: ";
-    cin >> num;
-    n = num; // assign number to n
-
-    do {
-    digit = num % 10; // set digit to LAST NUMBER OF NUM
-    rev = (rev * 10) + digit; // multiplies by 10 and adds the next digit to start reversing it 
-    num = num / 10; // Take away the last digit
-    } while (num !=0);
-
-    cout << "The reverse of the number is: " << rev << endl;
-
-    if (n == rev)
-        cout << " Number is a palindrome";
-    else
-        cout << "Number is not a palindrome";
+            else if (n != rev)
+                rev = 0;
+        }
+    }
     return 0;
 }
